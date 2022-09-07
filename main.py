6aa59f43d2ae56bc2a0b2b271fa852dfcf85ac60
Gustavo Henrique Silva <gustavo.silva394@fatec.sp.gov.br>
2022-09-07 17:27:02 -0300

    Teste

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import pandas as pd
 
 sg.theme('LightGreen1')
 
+##teste
+
 class First():
     def __init__():               
         layout_principal = [
@@ -11,11 +13,14 @@ class First():
         return sg.Window('FIRST', layout=layout_principal, margins=(50,50), finalize=True)
     
     def fun_matricula():
+        menu_cadastro = ['Adicionar', ['Turma', 'Professor']]
         layout_cadastro = [
+            [sg.Menu(menu_cadastro)]
             [sg.Text('Matrícula', expand_x=True, font=('Arial', 20)), sg.Input('', font=('Arial', 20), key='-MATRICULA-')],
             [sg.Text('Nome', expand_x=True, font=('Arial', 20)), sg.Input('', font=('Arial', 20), key='-NOME-')],
             [sg.Text('Senha', expand_x=True, font=('Arial', 20)), sg.Input('', font=('Arial', 20), key='-SENHA-')],
-            [sg.Text('Turma', expand_x=True, font=('Arial', 20)), sg.Combo('A B C D', expand_x=True, font=('Arial', 20), key='-TURMA-')],
+            [sg.Text('Turma', expand_x=True, font=('Arial', 20)), sg.Combo('A B C D', expand_x=True, font=('Arial', 20), key='-TURMA-'),
+            sg.Text('Cargo', expand_x=True, font=('Arial', 20)), sg.Combo('Aluno Professor Coordenador', expand_x=True, font=('Arial', 20), key='-TURMA-')],
             [sg.Button('Confirmar', expand_x=True, font=('Arial', 20))]
         ]
         return sg.Window('Matrícula', layout=layout_cadastro, margins=(10, 10), finalize=True)
