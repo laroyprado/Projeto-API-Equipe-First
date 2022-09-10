710ea49a6eba3b71ee9b07f8503c41b383ed7156
Gustavo Henrique Silva <gustavo.silva394@fatec.sp.gov.br>
2022-09-10 10:50:29 -0300

    main

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-import PySimpleGUI as sg
-import pandas as pd
+import PySimpleGUI as sg # Importação da ferramenta PySimpleGUI, após o comando "as" podemos definir o apelido "sg" para que seja mais rápido e facil usar a biblioteca
+import pandas as pd #Importação da ferramenta Pandas, definindo o apelido "pd"
 
-sg.theme('LightGreen1')
+sg.theme('LightGreen1') # Aqui definimos o tema que será usado no layout
 
-class First():
-    def fun_matricula():
-        layout_cadastro = [
+class First(): # Criação da classe
+    def fun_matricula(): # Criação da função
+        layout_cadastro = [ # Criação da tela, especificando tamanho, fonte e KEY que é fundamental para o funcionamento do programa
             [sg.Text('Matrícula', font=('Arial', 20)), sg.Input('', font=('Arial', 20), key='-MATRICULA-')],
             [sg.Text('Nome', font=('Arial', 20)), sg.Input('', font=('Arial', 20), key='-NOME-')],
             [sg.Text('Senha', font=('Arial', 20)), sg.Input('', font=('Arial', 20), key='-SENHA-')],
@@ -14,13 +14,19 @@ class First():
             [sg.Button('Confirmar', expand_x=True, font=('Arial', 20), key='-CONFIRMAR-')]
         ]
         return sg.Window('Cadastro', layout=layout_cadastro, margins=(10, 10), finalize=True)
+        # Aqui estamos criando o comando para que a janela seja criada quando a função for executada
 
     matricula = fun_matricula()
+    # Aqui estamos definindo a variavel "matricula" como respondavel pelo funcionamento da função "fun_matricula"
 
-    while True:
-        window, eventos, valores = sg.read_all_windows()
+    while True: # Este comando serve para iniciar uma repetição, para que o programa funcione continuamente
+        window, eventos, valores = sg.read_all_windows()        
+        # Este comando a cima serve para armazenar as informações de qual janela está aberta no momento, qual evento foi realizado e também qual valor foi inserido
+        # window = serve para especificar a janela que está aberta no momento
+        # eventos = serve para capturar alguma ação realizada na janela, como clicar em algum botão
+        # valores = serve para receber o valor de um input, ou alguma outra forma de o usuário inserir infromações
 
-        if window == matricula and eventos == sg.WINDOW_CLOSED:
-            break
+        if window == matricula and eventos == sg.WINDOW_CLOSED: # Aqui estamos especificando o que será feito quando clicar no X da janela sendo "sg.WINDOW_CLOSE" o evento
+            break # Este comando serve para encerrar uma repetição
 
-First()
+First() # Este comando serve para que a classe seja executada
