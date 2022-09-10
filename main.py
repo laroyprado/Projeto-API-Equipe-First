0db172e276bbce6f6019e8cee3104f46e9fb1069
Gustavo Henrique Silva <gustavo.silva394@fatec.sp.gov.br>
2022-09-10 08:49:53 -0300

    main

--- main.py
+++ main.py
@@ -4,22 +4,34 @@ import pandas as pd
 sg.theme('LightGreen1')
 
 class First():
-    def fun_matricula():
-        menu_cadastro = [
-            ['Adicionar', ['Turma', 'Cargo']],
+    def fun_login():
+        layout_login = [
         ]
-        layout_cadastro = [
-            [sg.Menu(menu_cadastro)],
-            [sg.Text('Matrícula', expand_x=True, font=('Arial', 20)), sg.Input('', font=('Arial', 20), key='-MATRICULA-')],
-            [sg.Text('Nome', expand_x=True, font=('Arial', 20)), sg.Input('', font=('Arial', 20), key='-NOME-')],
-            [sg.Text('Senha', expand_x=True, font=('Arial', 20)), sg.Input('', font=('Arial', 20), key='-SENHA-')],
-            [sg.Text('Time', expand_x=True, font=('Arial', 20)), sg.Combo('A B C D', expand_x=True, font=('Arial', 20), key='-TURMA-'),
-            sg.Text('Cargo', expand_x=True, font=('Arial', 20)), sg.Combo('Aluno Professor Coordenador', expand_x=True, font=('Arial', 20), key='-TURMA-')],
-            [sg.Button('Confirmar', expand_x=True, font=('Arial', 20))]
-        ]
-        return sg.Window('Matrícula', layout=layout_cadastro, margins=(10, 10), finalize=True)
+    
+    def fun_aluno():
+        layout_aluno =[
 
-    matricula, login = fun_matricula(), None
+        ]
+    def fun_instrutor():
+        layout_instrutor =[
+
+        ]
+    def fun_admin():
+        layout_admin =[
+        ]
+
+    def fun_matricula():
+        layout_cadastro = [
+            [sg.Text('Matrícula', font=('Arial', 20)), sg.Input('', font=('Arial', 20), key='-MATRICULA-')],
+            [sg.Text('Nome', font=('Arial', 20)), sg.Input('', font=('Arial', 20), key='-NOME-')],
+            [sg.Text('Senha', font=('Arial', 20)), sg.Input('', font=('Arial', 20), key='-SENHA-')],
+            [sg.Text('Time', size=(10), font=('Arial', 20)), sg.Input('', size=(10), font=('Arial', 20), key='-TIME-'),
+            sg.Text('Cargo', size=(10), font=('Arial', 20)), sg.Input('', size=(10), font=('Arial', 20), key='-TURMA-')],
+            [sg.Button('Confirmar', expand_x=True, font=('Arial', 20), key='-CONFIRMAR-')]
+        ]
+        return sg.Window('Cadastro', layout=layout_cadastro, margins=(10, 10), finalize=True)
+
+    matricula = fun_matricula()
 
     while True:
         window, eventos, valores = sg.read_all_windows()
