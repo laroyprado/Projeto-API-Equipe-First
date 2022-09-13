97f681dcda436a592d5faf296ccfec89fa0703e2
Caio Costa Santos <63621128+Caio-eng-gif@users.noreply.github.com>
2022-09-13 19:53:58 -0300

    Add files via upload
    Sprint 1

--- main_temp.py
+++ main_temp.py
@@ -16,11 +16,26 @@ class First():
         return sg.Window('Cadastro', layout=layout_cadastro, margins=(10, 10), finalize=True)
 
     matricula = fun_matricula()
-
+    
     while True:
-        window, eventos, valores = sg.read_all_windows() 
-
-        if window == matricula and eventos == sg.WINDOW_CLOSED:
+        window, eventos, valores = sg.read_all_windows()
+        excel_header = ['Matricula', 'Nome','Senha','Time','Turma']
+        cadastro_df = pd.DataFrame(data = pd.read_excel('arquivo.xlsx', engine='openpyxl'), columns=excel_header)
+    
+        
+        if window == matricula and eventos == sg.WINDOW_CLOSED: 
             break
-
+       
+        if window == matricula and eventos in ['-CONFIRMAR-']:
+            if valores['-MATRICULA-'] and valores['-NOME-'] and valores['-SENHA-'] and valores['-TIME-'] and valores['-TURMA-'] != '':
+                l = cadastro_df['Matricula'].count()
+                cadastro_df.loc[l+1] = ([valores['-MATRICULA-']] + [valores['-NOME-']] + [valores['-SENHA-']] + [valores['-TIME-']] + [valores['-TURMA-']])
+                writer = pd.ExcelWriter('arquivo.xlsx')
+                cadastro_df.to_excel(writer)
+                writer.save()
+                matricula.close()
+                matricula = fun_matricula()
+                sg.popup_quick("Cadastrado com sucesso!")
+            else:
+                sg.popup_quick("Necessario preencher todos os campos!")
 First()
