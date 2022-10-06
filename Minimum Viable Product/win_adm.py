9cf39dd3476d88bc9a956d620eb4a60894056b72
Cristine Cordeiro Gomes <cristine.gomes@fatec.sp.gov.br>
2022-10-05 21:13:46 -0300

    Win_adm.py
    Tela do ADM em andamento

---
+++ Minimum Viable Product/win_adm.py
@@ -0,0 +1,42 @@
+from tkinter.font import BOLD
+import PySimpleGUI as sg
+import pandas as pd
+
+sg.theme('LightGreen1')
+layout = [
+    [sg.Text('Usuário',font=('Arial', 15))],[sg.Input(font=('Arial', 15), key='usuário', size=(30,30))],
+    [sg.Text('Senha',font=('Arial', 15))], [sg.Input(font=('Arial', 15),key='senha', password_char='*', size=(30,30))],
+    [sg.Checkbox('Salvar Login',font=('Arial', 10))],
+    [sg.Button('Entrar',font=('Arial',17), key='entrar')]
+]
+
+janela = sg.Window('Tela de Login', layout=layout, margins=(20,20),finalize=True)
+
+while True:
+    eventos, valores = janela.read()
+    if eventos == sg.WINDOW_CLOSED:
+        break
+    if eventos == 'Entrar':
+        
+        df = pd.read_excel('arquivo.xlsx')
+        df.head()
+                
+        login = df['Matricula']
+        senha = df['Senha']
+         
+           
+    if valores ['usuário'] == '' or valores ['senha'] == '':
+        sg.popup_quick("Preencha Todos os campos")
+    else:
+        x = 0
+        for usuario in login:
+            if valores['usuário'] == str(usuario[x]):
+                if valores['senha'] == str(senha[x]):
+                    sg.popup_quick("Bem Vindo")
+                    
+                    x=0
+                    break
+                                
+            x += 1                
+        if x > 0:           
+            sg.popup_quick("Sua senha está errada")
